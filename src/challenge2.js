5d30b93f6c2f5ddd2075f1cb9cf60cc66d351a56
Uwem Akan Nkereuwem <uwemakan12@gmail.com>
2020-04-11 00:41:52 +0100

    challenge4

--- src/challenge2.js
+++ src/challenge2.js
@@ -1,12 +1,12 @@
 export const calculateSevereCasesByRequestedTime = (infectionsByRequestedTime) => {
-  const severeCasesByRequestedTime = Math.trunc(infectionsByRequestedTime * 0.15);
+  const severeCasesByRequestedTime = Math.floor(infectionsByRequestedTime * 0.15);
   return { severeCasesByRequestedTime };
 };
 
 export const calculateHospitalBedsByRequestedTime = (
   totalHospitalBeds, severeCasesByRequestedTime
 ) => {
-  const hospitalBedsByRequestedTime = Math.trunc(totalHospitalBeds * 0.35)
-                                        - severeCasesByRequestedTime;
+  const hospitalBedsByRequestedTime = Math.trunc((totalHospitalBeds * 0.35)
+                                        - severeCasesByRequestedTime);
   return { hospitalBedsByRequestedTime };
 };
